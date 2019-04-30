8c8a5d8db09927e0d568934863bb1aa7b261bcb1
Takuya Mukohira <takuya.mk96@gmail.com>
2019-04-30 11:26:55 +0900

    Remove useless module

--- examples/sos.rb
+++ examples/sos.rb
@@ -1,7 +1,7 @@
 require_relative '../remorse'
 
 if __FILE__ == $0
-  remorse = Remo::ReMorse.new do |config|
+  remorse = ReMorse.new do |config|
     config.token = ENV['NATURE_API_TOKEN']
     config.turn_on_signal_id = ENV['NATURE_SIGNAL_ON_ID']
     config.turn_off_signal_id = ENV['NATURE_SIGNAL_OFF_ID']
--- remorse.rb
+++ remorse.rb
@@ -1,36 +1,34 @@
 require 'faraday'
 
-module Remo
-  class ReMorse
-    attr_writer :token, :turn_on_signal_id, :turn_off_signal_id
+class ReMorse
+  attr_writer :token, :turn_on_signal_id, :turn_off_signal_id
 
-    def initialize(options = {})
-      options.each do |key, value|
-        instance_variable_set("@#{key}", value)
-      end
-      yield(self) if block_given?
+  def initialize(options = {})
+    options.each do |key, value|
+      instance_variable_set("@#{key}", value)
     end
+    yield(self) if block_given?
+  end
 
-    def dot
-      turn_on
-      turn_off
-    end
+  def dot
+    turn_on
+    turn_off
+  end
 
-    def dash(interval=0.5)
-      turn_on
-      sleep interval
-      turn_off
-    end
+  def dash(interval=0.5)
+    turn_on
+    sleep interval
+    turn_off
+  end
 
-    private
-    def turn_on
-      url = "https://api.nature.global/1/signals/#{@turn_on_signal_id}/send"
-      Faraday.new(url).post { |req| req.headers['Authorization'] = "Bearer #{@token}" }
-    end
+  private
+  def turn_on
+    url = "https://api.nature.global/1/signals/#{@turn_on_signal_id}/send"
+    Faraday.new(url).post { |req| req.headers['Authorization'] = "Bearer #{@token}" }
+  end
 
-    def turn_off
-      url = "https://api.nature.global/1/signals/#{@turn_off_signal_id}/send"
-      Faraday.new(url).post { |req| req.headers['Authorization'] = "Bearer #{@token}" }
-    end
+  def turn_off
+    url = "https://api.nature.global/1/signals/#{@turn_off_signal_id}/send"
+    Faraday.new(url).post { |req| req.headers['Authorization'] = "Bearer #{@token}" }
   end
 end
